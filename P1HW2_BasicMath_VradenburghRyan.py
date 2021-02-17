670cc0edac2f0e559e25cce4212a0e62bd86276a
vradenbr <78699750+vradenbr@users.noreply.github.com>
2021-02-18 02:12:04 +0430

    Add files via upload
    Added P1HW2 Basic Math program

---
+++ P1HW2_BasicMath_VradenburghRyan.py
@@ -0,0 +1,42 @@
+# Basic Math Exercise For Homework
+# 18Feb2021
+# CTI-110 P1HW2 - Basic Math
+# Ryan Vradenburgh
+#
+
+"""
+This program accepts two numbers as input
+
+The first operation converts the string entered to an integer
+
+The second operation prints the numbers that were entered
+
+The third operation adds the two numbers and prints the sum
+
+The fourth operation multiplies the two numbers and prints the product
+"""
+
+
+# Asking for the first number and convert to integer
+num1 = int(input('Please enter the first number: '))
+
+# Asking for the second number and convert to integer
+num2 = int(input('Please enter the second number: '))
+
+# Get some space
+print()
+
+# Print the first number
+print('The first number is: ', + num1)
+
+# Print the second number
+print('The second number is: ', + num2)
+
+# Get some space
+print()
+
+# Print the sum of the first and second numbers
+print( 'Sum of both numbers: ', + num1 + num2 )
+
+# Print the result of multiplying the first and second numbers
+print('Product of both numbers: ', + num1 * num2 )
