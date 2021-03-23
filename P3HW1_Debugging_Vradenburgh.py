b14c40defa06c79454e6b43867fe2914365f1120
vradenbr <78699750+vradenbr@users.noreply.github.com>
2021-03-22 20:32:35 -0400

    Add files via upload
    Added File for P3HW1

---
+++ P3HW1_Debugging_Vradenburgh.py
@@ -0,0 +1,42 @@
+# This program will calculate your letter grade when given a numerical score
+# 19 March 2021
+# Ryan Vradenburgh
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    # No need for F, anything less than a D is an F
+    
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    else:
+        if score >= B_score:
+            print('Your grade is: B')
+        else:
+            if score >= C_score:
+                print('Your grade is: C')
+            else:
+                if score >= D_score:
+                    print('Your grade is: D')
+                else:
+                    if score < D_score:
+                       print('Your grade is: F')
+                     
+
+
+
+
+
+
+
+# program start
+main()
